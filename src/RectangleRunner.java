82cec2eb8e0c0b638b4b0ae307dcd5bc5373cc4e
sample <sample@bths.edu>
2023-09-28 09:17:19 -0400

    initial

--- src/RectangleRunner.java
+++ src/RectangleRunner.java
@@ -3,26 +3,14 @@ public class RectangleRunner {
         Rectangle plot1 = new Rectangle(150, 200);
         Rectangle plot2 = new Rectangle(125);
         Rectangle plot3 = new Rectangle();
-        System.out.println("Plot 1 Length: " + plot1.getLength());
-        System.out.println("Plot 1 Width: " + plot1.getWidth());
-        System.out.println("Plot 1 Area: " + plot1.calculateArea());
-        System.out.println("Plot 2 Length: " + plot2.getLength());
-        System.out.println("Plot 2 Width: " + plot2.getWidth());
-        System.out.println("Plot 2 Area: " + plot2.calculateArea());
-        System.out.println("Plot 3 Length: " + plot3.getLength());
-        System.out.println("Plot 3 Width: " + plot3.getWidth());
-        System.out.println("Plot 3 Area: " + plot3.calculateArea());
+
         plot1.setWidth(75);
         plot2.setWidth(75);
         plot3.setWidth(75);
-        System.out.println("Plot 1 Length: " + plot1.getLength());
-        System.out.println("Plot 1 Width: " + plot1.getWidth());
-        System.out.println("Plot 1 Area: " + plot1.calculateArea());
-        System.out.println("Plot 2 Length: " + plot2.getLength());
-        System.out.println("Plot 2 Width: " + plot2.getWidth());
-        System.out.println("Plot 2 Area: " + plot2.calculateArea());
-        System.out.println("Plot 3 Length: " + plot3.getLength());
-        System.out.println("Plot 3 Width: " + plot3.getWidth());
-        System.out.println("Plot 3 Area: " + plot3.calculateArea());
+        int plot1Area = plot1.calculateArea();
+        int plot2Area = plot2.calculateArea();
+        int plot3Area = plot3.calculateArea();
+        int seedRequired = plot1Area + plot2Area + plot3Area;
+        System.out.println("The three plots require " + seedRequired + " square feet of seed");
     }
 }
